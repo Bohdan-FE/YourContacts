a43171ab45b690745a8f8e8e8047538ccd33489a
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-10-21 14:58:59 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,9 @@
 import { Container } from 'components/App/App.styled';
 import Header from 'components/Header/Header';
-import { Suspense, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { isLoggedInSelector } from 'redux/selectors';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
 function Layout() {
-  const navigate = useNavigate();
-  const isLoggedIn = useSelector(isLoggedInSelector);
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate('/login');
-    }
-  }, [navigate, isLoggedIn]);
-
   return (
     <>
       <Header />
