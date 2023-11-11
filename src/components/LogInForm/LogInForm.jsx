d86157f3f0f52b9fd12a57a9bbefcdb30e2ad761
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-11-11 17:49:06 +0200

    Update LogInForm.jsx

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loginThunk } from 'redux/thunk';
