da62f8d04f60374aa48c5c010f42ba375f0f761c
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-11-28 21:07:45 +0200

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,7 +1,10 @@
 import { useDispatch } from 'react-redux';
-import { Item } from './ContactListItem.styled';
 import PropTypes from 'prop-types';
 import { deleteContactThunk } from 'redux/thunk';
+import Typography from '@mui/material/Typography';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import CallIcon from '@mui/icons-material/Call';
+import { Box, ListItem, Button } from '@mui/material';
 
 export const ContactListItem = ({ contact }) => {
   const dispatch = useDispatch();
@@ -17,17 +20,34 @@ export const ContactListItem = ({ contact }) => {
     .join(' ');
 
   return (
-    <Item>
-      <p>
-        {contactName}:<span>{contact.number}</span>
-      </p>
-      <button
-        type="button"
-        onClick={() => dispatch(deleteContactThunk(contact.id))}
-      >
-        Delete
-      </button>
-    </Item>
+    <ListItem
+      sx={{
+        justifyContent: 'space-between',
+        padding: '8px 0',
+        borderBottom: '1px solid rgb(0 0 0 / 27%)',
+      }}
+    >
+      <Box sx={{ minWidth: '130px', overflow: 'hidden' }}>
+        <Typography sx={{ fontSize: '18px', fontWeight: '600' }}>
+          {contactName}
+        </Typography>
+        <Typography sx={{ fontSize: '16px', fontWeight: '400' }}>
+          {contact.number}
+        </Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', flexWrap: 'nowrap', width: '128px' }}>
+        <Button variant="outline" component="a" href={`tel:${contact.number}`}>
+          <CallIcon />
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => dispatch(deleteContactThunk(contact.id))}
+        >
+          <DeleteForeverIcon />
+        </Button>
+      </Box>
+    </ListItem>
   );
 };
 
